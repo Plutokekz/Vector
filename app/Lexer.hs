8cc426e6374b6390d9b36fcf8cc1ba9532297b53
Lukas Mahr <lukas@yousuckatprogramming.de>
2025-01-16 01:04:11 +0100

    Update to a Lexer combinator

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -1,4 +1,19 @@
-import Data.Char (isDigit)
+module Lexer where
+
+import Control.Applicative (Alternative (..))
+import Data.List (nub)
+
+type Offset = Int
+
+data LexerError i = LexerError {erOffset :: Offset, erError :: LexerErrorType i} deriving (Eq, Show)
+
+data LexerErrorType i
+  = EndOfInput
+  | Unexpected i
+  | Empty
+  | ExpectedEndOfFile i
+  | Expected i i
+  deriving (Eq, Show)
 
 data Token
   = PROGRAMM
@@ -43,23 +58,80 @@ data Token
   | LowerTriangular
   deriving (Show, Eq)
 
-type Lexer = String -> [Maybe Token] -> ([Maybe Token], String)
+-- define a newtype Lexer with kind i and a with a constructor Lexer with a function runLexer
+-- i is our input for the lexer so in most cases a Char
+-- a is our result type after we lexed somthing, would me a token in most cases
+newtype Lexer i a = Lexer {runLexer :: [i] -> Offset -> Either [LexerError i] (Offset, a, [i])}
 
-parseParrent :: Lexer
-parseParrent ('(' : xs) tokens = (tokens ++ [Just LParent], xs)
-parseParrent (')' : xs) tokens = (tokens ++ [Just RParrent], xs)
-parseParrent ('[' : xs) tokens = (tokens ++ [Just LSquareParrent], xs)
-parseParrent (']' : xs) tokens = (tokens ++ [Just RSquareParrent], xs)
+-- Divine how to apply a function to our lexer type
+-- Lexer p where p has type [i] -> Either [LexerError i] (a, [i])
+-- f :: a -> b
+-- we return a new Lexer with with f applied if the lexer befor was sucsessfull otherwise return a Left with the error
+instance Functor (Lexer i) where
+  fmap f (Lexer p) = Lexer $ \input offset ->
+    case p input offset of
+      Left err -> Left err
+      Right (offset', output, rest) -> Right (offset', f output, rest)
 
-parseNumber :: String -> Lexer
-parseNumber acc "" tokens = (tokens ++ [Nothing], "")
-parseNumber acc (x : xs) tokens
-  | isDigit x = parseNumber (acc ++ [x]) xs tokens
-  | x == '.' = parseDuble (acc ++ [x]) xs tokens
-  | otherwise = (tokens ++ [Just (INumber (read acc :: Integer))], xs)
+-- Define how we can combine a lexer with another lexer
+-- if sucsessfull apllie the otherone else return the error
+instance Applicative (Lexer i) where
+  pure a = Lexer $ \input offset -> Right (offset, a, input)
+  Lexer f <*> Lexer p = Lexer $ \input offset ->
+    case f input offset of
+      Left err -> Left err
+      Right (offset', f', rest) ->
+        case p rest offset' of
+          Left err -> Left err
+          Right (offset'', output, rest') -> Right (offset'', f' output, rest')
 
-parseDuble :: String -> Lexer
-parseDuble acc "" tokens = (tokens ++ [Nothing], "")
-parseDuble acc (x : xs) tokens
-  | isDigit x = parseDuble (acc ++ [x]) xs tokens
-  | otherwise = (tokens ++ [Just (FNumber (read acc :: Double))], xs)
+-- apply k to lexer with the lexeing of input was sucsessfully otherwise return the error
+-- where k har the type a -> Lexer i b
+instance Monad (Lexer i) where
+  return = pure
+  Lexer p >>= k = Lexer $ \input offset ->
+    case p input offset of
+      Left err -> Left err
+      Right (offset', output, rest) ->
+        let Lexer p' = k output
+         in p' rest offset'
+
+-- If the first lexer fails we use the alternative one otherwise we use the first one
+instance (Eq i) => Alternative (Lexer i) where
+  empty = Lexer $ \_ offset -> Left [LexerError offset Empty]
+
+  Lexer l <|> Lexer r = Lexer $ \input offset ->
+    case l input offset of
+      Left err ->
+        case r input offset of
+          Left err' -> Left $ nub $ err <> err'
+          Right (offset', output, rest) -> Right (offset', output, rest)
+      Right (offset'', output, rest) -> Right (offset'', output, rest)
+
+token :: (i -> LexerErrorType i) -> (i -> Bool) -> Lexer i i
+token mkErr predicate = Lexer $ \input offset ->
+  case input of
+    [] -> Left [LexerError offset EndOfInput]
+    hd : rest
+      | predicate hd -> Right (offset + 1, hd, rest)
+      | otherwise -> Left [LexerError offset $ mkErr hd]
+
+satisfy :: (i -> Bool) -> Lexer i i
+satisfy = token Unexpected
+
+
+char :: Eq i => i -> Lexer i i
+char i = token (Expected i) (== i)
+
+string :: Eq i => [i] -> Lexer i [i]
+string [] = return []
+string (x : xs) = do
+  y <- char x
+  ys <- string xs
+  return (y : ys)
+
+eof :: Lexer i ()
+eof = Lexer $ \input offset ->
+  case input of
+    [] -> Right (0, (), [])
+    hd : _ -> Left [LexerError offset $ ExpectedEndOfFile hd]
