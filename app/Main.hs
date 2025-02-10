75606d42647619069b55cebb3fe78021c0f70686
Lukas Mahr <lukas@yousuckatprogramming.de>
2025-02-10 15:25:53 +0100

    Remove unsupported Tokens from input

--- app/Main.hs
+++ app/Main.hs
@@ -23,7 +23,6 @@ z = "PROGRAMM matrixOps;\
     \  B: Identity[FLOAT64];\
     \  C: Diagonal[INT8];\
     \BEGIN\
-    \  -- Matrix initialization\
     \  A = [[1, 0, 3],\
     \       [0, 2, 0],\
     \       [4, 0, 5]];\
@@ -31,31 +30,25 @@ z = "PROGRAMM matrixOps;\
     \       [0, 1, 0],\
     \       [0, 0, 1]];\
     \  \
-    \  -- Matrix operations\
-    \  D = A @ B;           -- matrix multiplication\
-    \  E = A';              -- transpose\
-    \  F = A .+ B;          -- element-wise addition\
-    \  G = A .* B;          -- element-wise multiplication\
-    \  H = A .- B;          -- element-wise subtraction\
-    \  I = A ./ B;          -- element-wise division\
+    \  D = A @ B;           \
+    \  E = A';              \
+    \  G = A .* B;          \
+    \  I = A ./ B;          \
     \  \
-    \  -- Comparisons\
     \  IF A <= B THEN\
     \    WRITE A;\
     \  IF A >= B THEN\
     \    WRITE B;\
     \  \
-    \  -- Matrix types\
     \  J: LowerTriangular[INT32];\
     \  K: UpperTriangular[FLOAT64];\
-    \  L: Orthogonal[INT128];\
-    \"
+    \  L: Orthogonal[INT128];"
 
 main :: IO ()
 main = do
   let tokenx = tokenize x
   let tokeny = tokenize y
   let tokenz = tokenize z
-  print tokenx
-  print tokeny
+  --print tokenx
+  --print tokeny
   print tokenz
